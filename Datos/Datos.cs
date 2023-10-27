e6d80a9806064d436cb118c1b733f57d07a1e492
lololfoo <rodoemma12@gmail.com>
2023-10-27 10:52:17 -0700

    Agregar archivos de proyecto.

---
+++ Datos/Datos.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenED_EESA.Datos
+{
+    internal class Datos
+    {
+        public void uno()
+        {
+            int total = 0;
+            int total2 = 0;
+            int total3 = 0;
+            int total4 = 0;
+            //             0   1    2    3    4    5  6   7     8     9    10  11     0   1    2     3   4     5   6   7    8    9    10    11
+            int[,] a = { { 1, 500, 1000, 250, 400, 60, 2, 3000, 150, 1800, 130, 20}, { 3, 100, 200, 2900, 90, 4000, 4, 400, 500, 300, 2400, 3600 } };
+            Console.WriteLine($"El vendedor {a[0, 0]} tuvo ganancias de");
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine(a[0, i]);
+                total = total + a[0, i];
+            }
+            Console.WriteLine("\nCon un total de " + total);
+            Console.WriteLine($"\nEl vendedor {a[0, 6]} tuvo ganancias de");
+            for (int i = 7; i < 12; i++)
+            {
+                Console.WriteLine(a[0, i]);
+                total2 = total2 + a[0, i];
+            }
+            Console.WriteLine("\nCon un total de " + total2);
+            Console.WriteLine($"\nEl vendedor {a[1, 0]} tuvo ganancias de");
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine(a[1, i]);
+                total3 = total3 + a[1, i];
+            }
+            Console.WriteLine("\nCon un total de " + total3);
+            Console.WriteLine($"\nEl vendedor {a[1, 6]} tuvo ganancias de");
+            for (int i = 7; i < 12; i++)
+            {
+                Console.WriteLine(a[1, i]);
+                total4 = total4 + a[1, i];
+            }
+            Console.WriteLine("\nCon un total de " + total4);
+        }
+        public void dos()
+        {
+            char[] a = new char[18];
+            //012345678910
+            //SAAE040229H
+            Console.WriteLine("Dame tu curp letra por letra");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Char.Parse(Console.ReadLine());
+            }
+            switch (a[10])
+            {
+                case 'H':
+                    {
+                        Console.Write("Usted es de Sexo Masculino, ");
+                        break;
+                    }
+                case 'M':
+                    {
+                        Console.Write("Usted es de Sexo Femenino, ");
+                        break;
+                    }
+            }
+            Console.Write("y naciste el ");
+            for (int i = 8; i < 10; i++)
+            {
+                Console.Write(a[i]);
+            }
+            Console.Write("/");
+            for (int i = 6; i < 8; i++)
+            {
+                Console.Write(a[i]);
+            }
+            Console.Write("/");
+            for (int i = 4; i < 6; i++)
+            {
+                Console.Write(a[i]);
+            }
+
+        }
+        public void tres()
+        {
+            Random random= new Random();
+            int[] a = new int[100];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(0,100);
+                Console.WriteLine(a[i]);
+            }
+            //########################################
+            //Ordenamiento
+            int tmp = 0;
+
+            for(int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = 1; j < a.Length - i -1; j++)
+                {
+                        a[i] = tmp;
+                        tmp = a[j + 1];
+                        a[j + 1] = tmp;
+                }
+            }
+            Console.WriteLine("\n######################\n");
+            for(int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExamenED_EESA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Datos.Datos datos = new Datos.Datos();
+            //############################################################
+            /*Dada la siguiente tabla de productos por vendedores, 
+             * cree un programa que utilice un arreglo bidimensional 
+             * para calcular la ganancia de la venta por producto y 
+             * la ganancia generada por cada vendedor.*/
+            datos.uno();
+            //############################################################
+            /*Cree un programa que, dado el CURP de una persona, pueda 
+             * determinar su sexo y fecha de nacimiento.*/
+            //datos.dos();
+            //############################################################
+            /*Se tiene un arreglo unidimensional de 100 números enteros, 
+             * que representa las calificaciones de un grupo de 
+             * estudiantes. Los números están desordenados.*/
+            //datos.tres();
+            //############################################################
+        }
+    }
+}
